786a29fddb3a4cc70890f6accf179e42e2f604a7
sageMyers <97477501+sageMyers@users.noreply.github.com>
2024-02-20 13:53:30 -0600

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,7 @@
 from Interpreter import MyPythonInterpreter
 
 
-mypython_code = """write ( "Hello,MyPython!" ) """
+mypython_code = """write ( "Hello, World!" ) """
 
 interpreter = MyPythonInterpreter()
 interpreter.interpret(mypython_code)
